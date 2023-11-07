2dbdbcff2c304f12d8cc6c436345566484584af2
jisooshin <kelly6226@naver.com>
2023-11-07 14:39:38 +0900

    [FEAT] HomePage 디자인 완료

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import { COLORS } from 'colors';
 import React from 'react';
+import { Text } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -12,9 +13,38 @@ function HomeScreen(props) {
       <MainTxt>AppDoc</MainTxt>
       <Section>
         <ShowTime>
-          <TimeTxt>임박한 상담</TimeTxt>
+          <TotalTxt>
+            <TimeTxt style={{ marginBottom: 2 }}>임박한 상담</TimeTxt>
+            <TimeTxt>
+              <Text style={{ color: COLORS.subMiddleblue }}>01:00:59 </Text>남음
+            </TimeTxt>
+          </TotalTxt>
           <MaterialCommunityIcons name={'timer-outline'} color={COLORS.main} size={RFValue(50)} />
         </ShowTime>
+        <SecondRow>
+          <ManageConsult>
+            <Txt>상담 관리</Txt>
+          </ManageConsult>
+          <RightSection>
+            <MidBox style={{ marginBottom: RFValue(10) }}>
+              <Txt>예약 관리</Txt>
+            </MidBox>
+            <MidBox>
+              <Txt>의사 찾기</Txt>
+            </MidBox>
+          </RightSection>
+        </SecondRow>
+        <ThirdRow>
+          <SmallBox>
+            <Txt>커뮤니티</Txt>
+          </SmallBox>
+          <SmallBox>
+            <Txt>AI 상담</Txt>
+          </SmallBox>
+          <SmallBox>
+            <Txt>내 정보</Txt>
+          </SmallBox>
+        </ThirdRow>
       </Section>
     </Container>
   );
@@ -36,7 +66,6 @@ const BellIcon = styled(MaterialCommunityIcons)`
 const MainTxt = styled.Text`
   font-size: ${RFValue(35)}px;
   font-weight: bold;
-  margin-top: 20px;
   position: absolute;
   top: 110px;
 `;
@@ -44,9 +73,10 @@ const MainTxt = styled.Text`
 const Section = styled.View`
   justify-content: center;
   flex: 1;
+  margin-top: 150px;
 `;
 
-const ShowTime = styled.View`
+const ShowTime = styled.TouchableOpacity`
   border-color: ${COLORS.main};
   border-style: solid;
   border-width: 3px;
@@ -60,6 +90,80 @@ const ShowTime = styled.View`
   align-items: center;
 `;
 
-const TimeTxt = styled.Text``;
+const TimeTxt = styled.Text`
+  font-weight: bold;
+  color: ${COLORS.main};
+  font-size: ${RFValue(16)}px;
+`;
+
+const TotalTxt = styled.View`
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SecondRow = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: ${RFValue(40)}px;
+`;
+
+const ManageConsult = styled.TouchableOpacity`
+  border-color: ${COLORS.main};
+  border-style: solid;
+  border-width: 3px;
+  border-radius: 10px;
+  background-color: ${COLORS.subLightblue};
+
+  width: ${RFValue(160)}px;
+  height: ${RFValue(160)}px;
+
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Txt = styled.Text`
+  color: ${COLORS.white};
+  font-size: ${RFValue(16)}px;
+  font-weight: bold;
+`;
+
+const RightSection = styled.View``;
+
+const MidBox = styled.TouchableOpacity`
+  border-color: ${COLORS.subLightblue};
+  border-style: solid;
+  border-width: 3px;
+  border-radius: 10px;
+  background-color: ${COLORS.subMiddleblue};
+
+  width: ${RFValue(160)}px;
+  height: ${RFValue(75)}px;
+
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const ThirdRow = styled.View`
+  margin-top: ${RFValue(40)}px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const SmallBox = styled.TouchableOpacity`
+  border-color: ${COLORS.main};
+  border-style: solid;
+  border-width: 3px;
+  border-radius: 10px;
+  background-color: ${COLORS.main};
+
+  width: ${RFValue(105)}px;
+  height: ${RFValue(105)}px;
+
+  justify-content: space-around;
+  align-items: center;
+`;
 
 export default HomeScreen;
