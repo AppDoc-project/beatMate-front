11bb0ffcb3ffa6aa8e3aaa18712036cd53af09da
kelly6226 <kelly6226@naver.com>
2024-01-18 15:00:52 +0900

    [FEAT] 내 정보 api 함수들 추가

--- api/mypage.js
+++ api/mypage.js
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { client } from './client';
-import { useState } from 'react';
+
 // 프로필 조회 가져오기
 const getMyPageSection = async () => {
   try {
@@ -40,4 +40,61 @@ const getMyPostSection = async (page, limit) => {
   }
 };
 
-export { getMyPageSection, getMyPostSection };
+// 본인이 작성한 글 조회
+const getUserWritePost = async () => {
+  try {
+    const token = await AsyncStorage.getItem('access_token');
+    console.log(token); // 토큰 확인 (디버깅용)
+
+    const response = await client.post('/community/profile/post', {
+      headers: {
+        Authorization: token, // 바로 토큰 값 넣기
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// 본인이 작성한 댓글 조회
+const getUserCommentPost = async () => {
+  try {
+    const token = await AsyncStorage.getItem('access_token');
+    console.log(token); // 토큰 확인 (디버깅용)
+
+    const response = await client.post('/community/profile/thread', {
+      headers: {
+        Authorization: token, // 바로 토큰 값 넣기
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// 본인이 북마크한 글 조회
+const getUserBookmarkPost = async () => {
+  try {
+    const token = await AsyncStorage.getItem('access_token');
+    console.log(token); // 토큰 확인 (디버깅용)
+
+    const response = await client.post('/community/profile/bookmark', {
+      headers: {
+        Authorization: token, // 바로 토큰 값 넣기
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export { getMyPageSection, getMyPostSection, getUserWritePost, getUserCommentPost, getUserBookmarkPost };
