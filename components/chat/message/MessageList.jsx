2f82cf1f5aa2d912e89b364382b8abc6cb95850f
kelly6226 <kelly6226@naver.com>
2024-01-20 13:43:09 +0900

    [FEAT] 소켓 연결 중

--- components/chat/message/MessageList.jsx
+++ components/chat/message/MessageList.jsx
@@ -1,10 +1,12 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
 import { getMessage } from 'api/chat';
 import { UserInfo } from 'context/UserInfoContext';
 import format from 'pretty-format';
 import PropTypes from 'prop-types';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { FlatList, Text, View } from 'react-native';
+import socketio from 'socket.io-client';
 
 import MyMessage from './MyMessage';
 import OthersMessage from './OthersMessage';
@@ -15,12 +17,14 @@ function MessageList({ roomID }) {
     loginUserInfo: [loginUser],
   } = useContext(UserInfo);
 
-  const myUserId = loginUser.id; // 현재 내 id
+  const myUserId = loginUser.id;
 
   const [messages, setMessages] = useState([]);
   const [ChatData, setChatData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+
+  const socketRef = useRef(null);
 
   const loadMoreMessages = () => {
     if (!isLoading && !isError) {
@@ -29,7 +33,6 @@ function MessageList({ roomID }) {
         .then((res) => {
           setIsLoading(false);
           setChatData(res.data);
-          console.log(format(res.data));
           setMessages((prevMessages) => [...prevMessages, ...res.data.data]);
         })
         .catch((err) => {
@@ -40,11 +43,47 @@ function MessageList({ roomID }) {
     }
   };
 
+  const initializeSocket = async () => {
+    const token = await AsyncStorage.getItem('access_token');
+    console.log('Trying to initialize socket...');
+    console.log('Token:', token);
+
+    // 이전 소켓 연결이 있으면 해제
+    if (socketRef.current) {
+      socketRef.current.disconnect();
+    }
+
+    // 소켓 연결 초기화
+    socketRef.current = socketio(`http://192.168.1.157/room?roomId=${roomID}`, {
+      path: '/socket.io',
+      extraHeaders: {
+        Authorization: `${token}`,
+      },
+    });
+
+    // 소켓 이벤트 리스너 등록
+    socketRef.current.on('message', (newMessage) => {
+      console.log('newMessage', newMessage);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    });
+
+    // 언마운트 시 소켓 연결 해제
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+      }
+    };
+  };
+
   useFocusEffect(
     React.useCallback(() => {
-      loadMoreMessages();
+      initializeSocket();
     }, [roomID]),
   );
+
+  useEffect(() => {
+    loadMoreMessages();
+  }, [roomID]);
 
   if (isLoading) {
     return (
@@ -71,18 +110,18 @@ function MessageList({ roomID }) {
       keyExtractor={(message) => `message_${message.id}`}
       ListHeaderComponent={() => <DateInfo />}
       onEndReached={loadMoreMessages}
-      onEndReachedThreshold={0.1} // 조절이 필요하면 적절한 값으로 수정
-      inverted={true} // 화면을 역순으로 보여주도록 설정
+      onEndReachedThreshold={0.1}
+      inverted={true}
       renderItem={({ item, index }) =>
         item.sender.userId === myUserId ? (
           <MyMessage
-            key={`message_${item.id}`} // 이 부분을 추가해보세요
+            key={`message_${item.id}`}
             item={item}
             isLast={index !== messages.length - 1 ? item.sender.userId !== messages[index + 1].sender.userId : true}
           />
         ) : (
           <OthersMessage
-            key={`message_${item.id}`} // 이 부분을 추가해보세요
+            key={`message_${item.id}`}
             item={item}
             isLast={index !== messages.length - 1 ? item.sender.userId !== messages[index + 1].sender.userId : true}
           />
